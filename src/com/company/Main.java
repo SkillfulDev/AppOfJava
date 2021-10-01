a053bce436394d0ae7a163af6c4f2b73695ed91b
SkillfulDev <chernonog.evgeniy@gmail.com>
2021-10-01 05:33:24 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+
+    public static void main(String[] args)
+    {
+	    //sysytem.out.println("Hello");
+        System.out.println("Hello");
+    }
+}
